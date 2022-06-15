556326547ef27ca2995e8acfc090621cc230def7
BiagioCa <capuano.biagio@hotmail.com>
2022-06-15 14:38:24 +0200

    primo commit eclipse

---
+++ GitProject/src/main/MainTest.java
@@ -0,0 +1,13 @@
+package main;
+
+import classi.Persona;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		Persona p = new Persona("Biagio", "Capuano");
+		System.out.println(p.toString());
+
+	}
+
+}
